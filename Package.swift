ff3d58c7bba0e3a2244bce2802274ab185cdf682
Eonist <30n1st@gmail.com>
2017-10-01 16:49:26 +0200

    update

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+   	name: "swift-utils",
+	products: [
+        	.library(
+           	 name: "swift-utils",
+           	 targets: [""]
+       	 )
+   	 ],
+	dependencies:[],
+	targets: [
+		.target(
+            	name: "swift-utils",
+            	dependencies: []
+        	)
+	]
+)
